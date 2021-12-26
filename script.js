7adb93b21c5c1ca3c9cad61d37ea531298b50b84
itush <itusharbiswas@gmail.com>
2021-12-27 02:05:19 +0530

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+function share(){
+  
+var s = document.getElementById("social")
+    ;
+
+if (s.style.visibility === "visible") 
+
+    {
+    s.style.visibility = "hidden";
+  } 
+  
+else {
+    s.style.visibility = "visible";
+  }
+    
+    }
